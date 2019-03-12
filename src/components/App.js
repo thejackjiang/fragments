e4d3e9be9d1928e8c86baf9fb5564f8d28f42128
Jack Jiang <thejackjiang@gmail.com>
2019-03-12 02:16:09 -0400

    added list

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,43 @@
 import React, { Component } from 'react'
-import { gql } from 'apollo-boost'
+// import { gql } from 'apollo-boost'
 import { Query } from 'react-apollo'
-import logo from '../logo.svg'
-import '../styles/App.css'
-import InputName from './InputName'
+import gql from 'graphql-tag';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Query query={HELLO_QUERY}>
-            {props => {
-              console.log(props)
-              const { data, loading, error, refetch } = props
-              if (loading) {
-                return <div>Loading</div>
-              }
-
-              if (error) {
-                return <div>An unexpected error occurred</div>
-              }
-
-              return (
-                <div>
-                  <p>What's your name?</p>
-                  <InputName
-                    onSubmit={name => {
-                      refetch({
-                        name,
-                      })
-                    }}
-                  />
-                  <h3>{data.hello}</h3>
-                </div>
-              )
-            }}
-          </Query>
-        </div>
+        <Query query={HELLO_QUERY}>
+          {props => {
+            const { data: { allUsers }, loading, error, refetch } = props
+            let mapped;
+            if (allUsers) {
+              mapped = allUsers.map((elm) => {
+                return (<div>
+                  <h1>{elm.firstName}</h1>
+                  <h1>{elm.lastName}</h1>
+                </div>)
+              })
+            }
+            return (
+              <div>
+                <h3>{mapped}</h3>
+              </div>
+            )
+          }}
+        </Query>
       </div>
     )
   }
 }
 
 const HELLO_QUERY = gql`
-  query HelloQuery($name: String) {
-    hello(name: $name)
+{
+  allUsers{
+    firstName
+    lastName
   }
+}
 `
 
 export default App
